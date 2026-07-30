9a3afe586af152e4293214eebc713e11ba209e9a
rb077858 <165806112+rb077858@users.noreply.github.com>
2026-07-30 18:22:10 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -7,11 +7,12 @@
 // security rules (see firebase-rules.json).
 // ============================================================
 const firebaseConfig = {
-  apiKey: "REPLACE_WITH_YOUR_API_KEY",
-  authDomain: "REPLACE_WITH_YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://REPLACE_WITH_YOUR_PROJECT-default-rtdb.firebaseio.com",
-  projectId: "REPLACE_WITH_YOUR_PROJECT",
-  storageBucket: "REPLACE_WITH_YOUR_PROJECT.appspot.com",
-  messagingSenderId: "REPLACE_WITH_YOUR_SENDER_ID",
-  appId: "REPLACE_WITH_YOUR_APP_ID"
+  apiKey: "AIzaSyD0hbI667K1P2FNU1KqINm8MPL1JtdvE30",
+  authDomain: "anonchat-91d21.firebaseapp.com",
+  databaseURL: "https://anonchat-91d21-default-rtdb.firebaseio.com",
+  projectId: "anonchat-91d21",
+  storageBucket: "anonchat-91d21.firebasestorage.app",
+  messagingSenderId: "617121252083",
+  appId: "1:617121252083:web:cfcdf4f719f68d70cd2d86",
+  measurementId: "G-MKME24SFNZ"
 };
